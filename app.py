b82338a41f66711ff715b83b94a33d083e4b569b
ChaeRin01 <cofls8495@gmail.com>
2025-09-23 02:44:04 +0900

    hw1 작성
    -Problem Statement Canvas
    -솔루션 3줄
    -기능 명세
    -간단한 UX 흐름
    -와이어 프레임(스케치)

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def home():
     return render_template("home.html", title="홈")
 
+@app.get("/hw1")
+def hw1():
+    return render_template("hw1.html")
+
 if __name__=="__main__":
     app.run() #기본 http://127.0.0.1:5000
